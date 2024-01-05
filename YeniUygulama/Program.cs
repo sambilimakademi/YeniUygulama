610ce9d990c3b92abbd14d20c82f990b6dfdcd4e
sambi <antalyaeniyi@gmail.com>
2024-01-05 17:33:43 +0300

    Proje dosyası ekle.

---
+++ YeniUygulama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeniUygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int a = 0; a<10;a++)
+            {
+                Console.WriteLine("djksjkdjf");
+            }
+        }
+    }
+}
